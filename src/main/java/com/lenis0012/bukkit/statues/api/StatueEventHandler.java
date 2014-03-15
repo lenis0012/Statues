acc384b45cadd79b777e3bd25b9c228626e4feca
Lennart ten Wolde <0p1q9o2w@hotmail.nl>
2014-03-15 16:15:59 +0100

    Check if event has handlers

--- src/main/java/com/lenis0012/bukkit/statues/api/StatueEventHandler.java
+++ src/main/java/com/lenis0012/bukkit/statues/api/StatueEventHandler.java
@@ -3,13 +3,18 @@ package com.lenis0012.bukkit.statues.api;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import com.bergerkiller.bukkit.common.utils.CommonUtil;
 import com.lenis0012.bukkit.statues.core.Statue;
 
 public class StatueEventHandler {
 	
 	public static boolean callCreateEvent(Player player, Statue statue) {
 		StatueCreateEvent event = new StatueCreateEvent(statue, player);
-		Bukkit.getPluginManager().callEvent(event);
-		return !event.isCancelled();
+		if(CommonUtil.hasHandlers(event.getHandlers())) {
+			Bukkit.getPluginManager().callEvent(event);
+			return !event.isCancelled();
+		} else {
+			return true;
+		}
 	}
 }
